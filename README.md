a5d6d1ac3cf55d4174b7682dfc76a7c3891d53bf
Carlos Longhi <carloslonghi.cl@gmail.com>
2024-03-12 23:29:03 -0300

    feat: create import CSV using csv-parse, and refactor

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Tasks API
 
-Criar uma API para realizar o CRUD de suas *tasks* (tarefas).
+Criar uma API para realizar o CRUD de suas _tasks_ (tarefas).
 
 A API deve conter as seguintes funcionalidades:
 
@@ -9,4 +9,4 @@ A API deve conter as seguintes funcionalidades:
 - [x] Deve ser possível Atualizar uma task pelo `id`
 - [x] Deve ser possível Remover uma task pelo `id`
 - [x] Deve ser possível Marcar pelo `id` uma task como completa
-- [ ] Deve ser possível Importar as tasks em massa por um arquivo CSV
+- [x] Deve ser possível Importar as tasks em massa por um arquivo CSV
--- src/database.js
+++ src/database.js
@@ -1,65 +1,67 @@
-import fs from "node:fs/promises"
+import fs from 'node:fs/promises'
 
-// Ao usar "type: module" as funções __dirname e __filename não existem mais. Por isso a melhor forma é essa:
 const databasePath = new URL('../db.json', import.meta.url)
 
 export class Database {
-	#database = {}
+  #database = {}
 
-    constructor() {
-        fs.readFile(databasePath, 'utf-8')
-        .then(data => {
-            this.#database = JSON.parse(data)
-        })
-        .catch(() => {
-            this.#persist()
-        })
-    }
-
-    #persist() {
-        fs.writeFile(databasePath, JSON.stringify(this.#database))
-    }
-    
-    select (table, search) {
-        let data = this.#database[table] ?? []
-
-        if (search) {
-            data = data.filter(row => {
-                return Object.entries(search).some(([key, value]) => {
-                    return row[key].toLowerCase().includes(value.toLowerCase())
-                })
-            })
-        }
-
-        return data
-    }
-
-    insert (table, data) {
-        if (Array.isArray(this.#database[table])) {
-            this.#database[table].push(data)
-        } else {
-            this.#database[table] = [data]
-        }
-
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
         this.#persist()
-        return data
+      })
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database, null, 2))
+  }
+
+  select(table, search) {
+    let data = this.#database[table] ?? []
+
+    if (search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          if (!value) return true
+
+          return row[key].includes(value)
+        })
+      })
     }
 
-    update (table, id, data) {
-        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+    return data
+  }
 
-        if (rowIndex > -1) {
-            this.#database[table][rowIndex] = { id, ...data}
-            this.#persist()
-        }
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
+    } else {
+      this.#database[table] = [data]
     }
 
-    delete (table, id) {
-        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+    this.#persist()
+    return data
+  }
 
-        if (rowIndex > -1) {
-            this.#database[table].splice(rowIndex, 1)
-            this.#persist()
-        }
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      const row = this.#database[table][rowIndex]
+      this.#database[table][rowIndex] = { id, ...row, ...data }
+      this.#persist()
     }
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
+  }
 }
